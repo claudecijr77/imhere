5fdac02802a7e9d24292271d8ca38fcb209446ee
Claudeci <cast.claudeci.utic@sebrae.com.br>
2024-05-06 15:26:02 -0300

    Entendendo o JSX

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Claudeci Júnior</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View>
+        <Text>React</Text>
+        <Text>Native</Text>
+      </View>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
