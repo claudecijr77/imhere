05bfbe26b207994a998b10dca079937a5aabd076
Claudeci <cast.claudeci.utic@sebrae.com.br>
2024-05-08 15:01:56 -0300

    Criando o Form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -17,18 +17,20 @@ export function Home() {
                 Sexta, 4 de Novembro de 2024.
             </Text>
 
-            <TextInput
-                style={styles.input}
-                placeholder="Nome do participante"
-                placeholderTextColor="#6B6B6B"
-                // keyboardType="email-address"
-            />
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nome do participante"
+                    placeholderTextColor="#6B6B6B"
+                    // keyboardType="email-address"
+                />
 
-            <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-                <Text style={styles.buttonText}>
-                    +
-                </Text>
-            </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                    <Text style={styles.buttonText}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
 
         </View>
     )
