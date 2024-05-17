9361e4f66dff9d84111fdf77ec9541461daa2757
Claudeci <cast.claudeci.utic@sebrae.com.br>
2024-05-17 15:39:32 -0300

    Salvando participante no estado

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,14 +6,15 @@ import {Participant} from "../../components/Participant";
 
 export function Home() {
 
-    const [participants, setParticipants] = useState(['Claudeci'])
+    const [participants, setParticipants] = useState<string[]>([])
+    const [participantName, setParticipantName] = useState('')
 
     function handleParticipantAdd() {
-        if (participants.includes("Rodrigo")) {
+        if (participants.includes(participantName)) {
             return Alert.alert("Participante existe", "Já existe um participante na lista com esse nome.")
         }
-        setParticipants(prevState => [...prevState, 'Ana'])
-        console.log(participants)
+        setParticipants(prevState => [...prevState, participantName]);
+        setParticipantName('');
     }
 
     function handleParticipantRemove(name: string) {
@@ -44,6 +45,8 @@ export function Home() {
                     style={styles.input}
                     placeholder="Nome do participante"
                     placeholderTextColor="#6B6B6B"
+                    onChangeText={setParticipantName}
+                    value={participantName}
                     // keyboardType="email-address"
                 />
 
